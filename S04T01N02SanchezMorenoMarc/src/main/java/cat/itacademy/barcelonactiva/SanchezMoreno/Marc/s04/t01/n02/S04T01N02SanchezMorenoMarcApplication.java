15c51e393ab5cc985716d29cc06dc1c5d18c3543
Marc Sanchez Moreno <127627232+Wrennandsix@users.noreply.github.com>
2024-01-23 13:11:55 +0100

    Add files via upload

--- S04T01N02SanchezMorenoMarc/src/main/java/cat/itacademy/barcelonactiva/SanchezMoreno/Marc/s04/t01/n02/S04T01N02SanchezMorenoMarcApplication.java
+++ S04T01N02SanchezMorenoMarc/src/main/java/cat/itacademy/barcelonactiva/SanchezMoreno/Marc/s04/t01/n02/S04T01N02SanchezMorenoMarcApplication.java
@@ -2,10 +2,8 @@ package cat.itacademy.barcelonactiva.SanchezMoreno.Marc.s04.t01.n02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"controllers"})
 public class S04T01N02SanchezMorenoMarcApplication {
 
 	public static void main(String[] args) {
